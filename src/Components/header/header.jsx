04f526f3969f7ec832be81aaf9fcfe9f3ed89806
ScreameRRR <68212647+ScreameRRR@users.noreply.github.com>
2022-10-23 18:06:38 +0300

    header works

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,8 +1,17 @@
 import './header.css'
+import Logo from '../../images/logoMobile.png'
+import Burger from '../../images/hamburger-menu.svg'
 
 const Header = () => {
     return (
-        <header className='container header'></header>
+        <header className='container header'>
+            <nav>
+                <ul className='d-flex'>
+                    <li className='logo-header'><a href="#"><img className='logo-header' src={Logo} alt="Logo" /></a></li>
+                    <li><button><svg width="10px" height="10px"><use href={Burger}></use></svg></button></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
