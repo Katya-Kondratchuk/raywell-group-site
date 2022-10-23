d3deb158c9516c73b7dc0dfdaa34da499b03edf9
ScreameRRR <68212647+ScreameRRR@users.noreply.github.com>
2022-10-23 15:13:47 +0300

    header component added

---
+++ src/Components/header/header.jsx
@@ -0,0 +1,9 @@
+import './header.css'
+    
+const Header = () => {
+    return (
+        <div className="header">Lorem ipsum dolor sit amet.</div>
+    )
+}
+
+export default Header;
--- src/Components/slider/Slider.jsx
+++ src/Components/slider/Slider.jsx
@@ -1,10 +1,10 @@
 import 'react-slideshow-image/dist/styles.css'
 import React from 'react';
 import { Zoom } from 'react-slideshow-image';
-import envie from '../images/envie.jpg'
-import extremo from '../images/extremo.jpg'
-import invidia from '../images/invidia.jpg'
-import raywell from '../images/raywell.jpg'
+import envie from '../../images/envie.jpg'
+import extremo from '../../images/extremo.jpg'
+import invidia from '../../images/invidia.jpg'
+import raywell from '../../images/raywell.jpg'
 
 
 const images = [ envie, extremo, invidia, raywell];
@@ -16,7 +16,7 @@ return (
       <div className="slide-container">
         <Zoom scale={0.4}>
           {
-            images.map((each, index) => <img key={index} style={{width: "100%"}} src={each} />)
+            images.map((each, index) => <img alt='lol' key={index} style={{width: "100%"}} src={each} />)
           }
         </Zoom>
       </div>
