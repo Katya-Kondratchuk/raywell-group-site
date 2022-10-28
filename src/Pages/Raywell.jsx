9442715458071e05b0ac7e1671a0c3901c1265c2
MrScreamer <nostrees@ukr.net>
2022-10-28 05:28:16 +0300

    raywell added

---
+++ src/Pages/Raywell.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Contact from "../components/Contact/Contact.jsx";
+
+const Reywell = () => {
+    return (
+        <div>
+            <Header />
+            <Contact />
+           </div>
+  );
+};
+
+export default Reywell;
