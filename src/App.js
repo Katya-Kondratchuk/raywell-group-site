bcc91e42936034eac509909e75fe51184e125037
MrScreamer <nostrees@ukr.net>
2022-10-29 11:32:13 +0300

    oxygen completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Technics from "./Pages/Technics";
 import EnviePage from "./Pages/EnviePage";
 import Powder from "./Pages/Powder";
+import Oxygen from "./Pages/Oxygen";
+
 
 
 const App = () => {
@@ -13,6 +15,7 @@ const App = () => {
       <Route path="raywell" element={<Raywell />}></Route>
       <Route path="technics" element={<Technics />} />
       <Route path="powder" element={<Powder />} />
+      <Route path="oxygen" element={<Oxygen />} />
       <Route path="envie" element={<EnviePage />} />
       {/* <Route path="invidia" element={<Invidia />} /> */}
       {/* <Route path="*" element={<NotFound />} /> */}
