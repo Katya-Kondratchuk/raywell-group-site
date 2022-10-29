a4d3d305f3ef7b87f29574e20d997332f4b637a8
MrScreamer <nostrees@ukr.net>
2022-10-29 06:32:09 +0300

    powder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Raywell from "./Pages/Raywell";
 import { Routes, Route } from "react-router-dom";
 import Technics from "./Pages/Technics";
 import EnviePage from "./Pages/EnviePage";
+import Powder from "./Pages/Powder";
+
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="raywell" element={<Raywell />}></Route>
       <Route path="technics" element={<Technics />} />
+      <Route path="powder" element={<Powder />} />
       <Route path="envie" element={<EnviePage />} />
       {/* <Route path="invidia" element={<Invidia />} /> */}
       {/* <Route path="*" element={<NotFound />} /> */}
---
+++ src/Components/Powder/PowderComp.jsx
@@ -0,0 +1,16 @@
+import './Powder.css';
+import Pudres from '../../images/pudres.jpg'
+
+const PowderComp = () => {
+    return (
+        <div>
+        <h1></h1>
+            <span></span>
+            <details>lololo</details>
+        </div>
+    )
+}
+
+
+
+export default PowderComp;
