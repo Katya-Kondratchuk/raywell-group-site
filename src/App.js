7e5584bfdbdfd8f0071873aa81381d9a40b850d9
MrScreamer <nostrees@ukr.net>
2022-10-29 05:28:46 +0300

    technics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Home from "./pages/Home";
-import Reywell from "./pages/Raywell";
+import Home from "./Pages/Home";
+import Raywell from "./Pages/Raywell";
 import { Routes, Route } from "react-router-dom";
-// import Technics from "./pages/Technics";
-import EnviePage from "./pages/EnviePage";
+import Technics from "./Pages/Technics";
+import EnviePage from "./Pages/EnviePage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="raywell" element={<Reywell />}></Route>
-      {/* <Route path="technics" element={<Technics />} /> */}
+      <Route path="raywell" element={<Raywell />}></Route>
+      <Route path="technics" element={<Technics />} />
       <Route path="envie" element={<EnviePage />} />
       {/* <Route path="invidia" element={<Invidia />} /> */}
       {/* <Route path="*" element={<NotFound />} /> */}
---
+++ src/Pages/Technics.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header/Header";
+import Contact from "../components/Contact/Contact";
+import TechnicsComp from "../components/Technics/Technics";
+
+
+
+const Technics = () => {
+    return (
+        <div>
+         <Header />
+         <TechnicsComp />
+         <Contact />
+        </div>
+            )
+}
+
+export default Technics;
